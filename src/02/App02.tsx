c3457bd8af098bda2adff1a787b4f02852a62670
YannOuafete <yannouafete@gmail.com>
2025-02-06 17:22:33 +0100

    nouvelle modifcation

--- src/02/App02.tsx
+++ src/02/App02.tsx
@@ -1,7 +1,16 @@
+import Capital  from './capital';
+import Etudiant  from './Etudiant';
+import Bienvenue  from './Bienvenue';
+
 export default function App02(){
     return(
-        <div >
-           Bonjour
+        <div>
+              
+              <Bienvenue lang='en' nom='Yann' prenom='OUAFETE' />
+              <Bienvenue lang='fr' nom='Juvence' prenom='OUAFETE' />
+              <Bienvenue lang='es' nom='Alice' prenom='Deninga' />
+             
         </div>
+      
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
-import App02  from './02/App02'
+import App  from './02/App02'
 
 const div = document.getElementById('root');
 const root = createRoot(div!);
-root.render(<App02/>)
+root.render(<App />)
 
