5e1a1d13f6a450b3f18606154055b34e0e4ab26c
YannOuafete <yannouafete@gmail.com>
2025-02-06 15:13:00 +0100

    ajout du dossier O2

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import App01  from './01/App01'
 
-createRoot(document.getElementById('root')!).render(<App01/>)
+const div = document.getElementById('root');
+const root = createRoot(div!);
+root.render(<App01/>)
+
